6ecb8e2b875b30eb0918e60aa4eeaaf3865e1206
Thomas <tomandrewmills@gmail.com>
2016-04-06 00:06:59 +0100

    Setup test

--- index.php
+++ index.php
@@ -32,10 +32,12 @@
                     <br>
                     <span class="subtitle">
                         <?php
-                        $connection = getConnectionWithAccessToken("455303569-USh4o67B8c6vYzw8taIp4G6WtjQoxpMOS8rseRNI", "lIrlNxvTW6TY5DBUJmn7VuiWlMtAPz4tUrLE5ngUpENRL");
-                        $content = $connection->
+                        //$connection = getConnectionWithAccessToken("455303569-USh4o67B8c6vYzw8taIp4G6WtjQoxpMOS8rseRNI", "lIrlNxvTW6TY5DBUJmn7VuiWlMtAPz4tUrLE5ngUpENRL");
+                        //$content = $connection->
 
-                        get("statuses/home_timeline");
+                        //get("statuses/home_timeline");
+
+                        echo("Test!");
                         ?>
                     </span>
                     <br>
