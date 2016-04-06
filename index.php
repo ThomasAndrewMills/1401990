e111a23f6b8bbf91b17b7f6c80bd05c8b51d0b32
Thomas <tomandrewmills@gmail.com>
2016-04-06 15:58:20 +0100

    Setup test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!--DARREN WILLIAMSON, THOMAS MILLS, CALLUM MELIA, CALLUM ROBERTSON, CIARAN MILLER-->
 <html>
     <head>
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
         <script src="map.js"></script>
         <title>
             trendingWhere?
