e275efca9b44c673d1a953c319fbd2054a971e2b
Thomas <tomandrewmills@gmail.com>
2016-04-06 15:31:54 +0100

    Setup test

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!--DARREN WILLIAMSON, THOMAS MILLS, CALLUM MELIA, CALLUM ROBERTSON, CIARAN MILLER-->
 <html>
     <head>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="map.js"></script>
         <title>
             trendingWhere?
@@ -12,7 +13,7 @@
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBPq8HpLPgbIWth84LJfbbR4jsCJkrasuc&libraries=visualization&callback=initMap">
         </script>
 
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
         <script>
             $(document).ready(function(){
                 $(".signintwitter").click(function(){
