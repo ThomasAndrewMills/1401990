e71acc3812c03e3d1ad6630bc98afc3af201fe2d
Thomas <tomandrewmills@gmail.com>
2016-04-23 16:43:07 +0100

    Testing twitter proxy

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                     <span class="subtitle">
                         <script>
                             $.getJSON('/twitter-proxy.php?url='+encodeURIComponent('statuses/user_timeline.json?screen_name=MikeRogers0&count=2'), function(d){
-                                // Some magic here
+                                var str = JSON.stringify(obj, null, 2); // spacing level = 2
                             }
                         </script>
                         Sign in using your twitter account and explore the trending topics<br> of cities around the world!
